450e27530ba3e9d0518260b8271c087b2501d8b1
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-08-09 15:53:07 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYQOWc-LEpzmB7tOZL5yZyCuehoQAX4nQ",
+  authDomain: "vue-training-8b8bf.firebaseapp.com",
+  projectId: "vue-training-8b8bf",
+  storageBucket: "vue-training-8b8bf.appspot.com",
+  messagingSenderId: "820220288948",
+  appId: "1:820220288948:web:f0e53fc5c09ee7af9bdce1",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(store).use(router).mount("#app");
