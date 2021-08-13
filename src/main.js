5bf9fb593182f10ac8943618a7f7c809f58ddc07
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-08-13 15:22:46 +0200

    moved API key to seperate file

--- src/main.js
+++ src/main.js
@@ -4,14 +4,17 @@ import router from "./router";
 import store from "./store";
 import firebase from "firebase/app";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBYQOWc-LEpzmB7tOZL5yZyCuehoQAX4nQ",
+  apiKey: `${process.env.VUE_APP_API_KEY}`,
   authDomain: "vue-training-8b8bf.firebaseapp.com",
   projectId: "vue-training-8b8bf",
   storageBucket: "vue-training-8b8bf.appspot.com",
   messagingSenderId: "820220288948",
   appId: "1:820220288948:web:f0e53fc5c09ee7af9bdce1",
 };
+
+console.log(firebaseConfig)
 
 firebase.initializeApp(firebaseConfig);
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,45 @@
 <template>
   <div class="home">
-    <h1>Home page</h1>
+    <h1>Welcome, {{ name }}</h1>
+
+    <router-link to="/about">About</router-link>
+    <br>
+    <button @click="Logout">Logout</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { ref, onBeforeMount } from 'vue'
+import firebase from 'firebase/app'
+import 'firebase/auth';
 
 export default {
   name: "Home",
+  setup() {
+    const name = ref("");
+
+    onBeforeMount( () => {
+      const user = firebase.auth().currentUser;
+
+      if(user) {
+        name.value = user.email.split('@')[0]
+      }
+
+    })
+
+    const Logout = () => {
+      firebase
+        .auth()
+        .signOut()
+        .then( () => { console.log('Signed Out')})
+        .catch( err => alert(err.message))
+    }
+
+    return {
+      name,
+      Logout
+    }
+  }
 };
 </script>
